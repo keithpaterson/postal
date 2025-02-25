f8a2e264464ebe8028b9339478cf396b9871edd4
Keith Paterson <keith.w.paterson@gmail.com>
2025-02-25 17:55:58 -0500

    scripts: initial pkg and scripts/ config

---
+++ scripts/_build.sh
@@ -0,0 +1,47 @@
+using colors
+
+_bin_dir=${_root_dir}/bin
+
+run_build_usage() {
+  echo "  where $(color -bold op) is:"
+  echo "    $(color -lt_green \<empty\>) : build the app"
+  echo "    $(color -lt_green test)    : build the app for test"
+}
+
+run_build() {
+  local _op=$1
+  shift
+  case ${_op} in
+    -h|--help|help)
+      run_handler_usage build
+      exit 1
+      ;;
+    ""|postal)
+      _build_app $*
+      ;;
+    generate)
+      _build_generate $*
+      ;;
+    test)
+      echo "Build for test ..."
+      go build -tags testutils ./... $*
+      ;;
+    *)
+      error "Unrecognized $(color -bold op): '${_op}'"
+      exit 1
+      ;;
+  esac
+}
+
+_build_app() {
+  echo "Build ..."
+  mkdir -p ${_bin_dir}
+  #cd ${_root_dir}/${_name}
+  # _version=$(_run bump -r ${_root_dir} -d -b alpha -s)
+  # -ldflags="-X 'main.version=${_version}'"
+  go build -o ${_bin_dir}/postal $* ${_root_dir}/...
+}
+
+_build_generate() {
+  go generate ./...
+}
