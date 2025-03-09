5997245a143cccbda61a4e00b4dc371964565602
Keith Paterson <keith.w.paterson@gmail.com>
2025-03-09 17:24:54 -0400

    update JWT doc to explain signing-key input format

--- jwt/doc.go
+++ jwt/doc.go
@@ -14,5 +14,14 @@
 //
 // You should prefer passing the key in at the command-line in order to better protect it.
 //
+// The signing key value specified in configs or on the command-line must conform to the format:
+//
+//		 "type:value", where type can be one of:
+//		   "string"  : the value is a text string.  Note that using "value" implies a string key
+//		   "hex"     : the value is a space-separated list of hexadecimal values,
+//	                e.g. "hex:01 02 03 04 05" -> "[0x01, 0x02, 0x03, 0x04, 0x05]"
+//		   "pemfile" : the value is a PEM filename
+//		   "pemdata" : the value is a PEM signature (e.g. the value you would find in the PEM file)
+//
 // Assuming no errors, 'token' will contain the complete, signed JWT token.
 package jwt
