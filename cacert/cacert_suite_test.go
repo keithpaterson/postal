d81a54a2ae6eb852866ceb15e8549d01a399f6f5
Keith Paterson <keith.w.paterson@gmail.com>
2025-03-13 15:18:01 -0400

    cacert: add builder/parser

---
+++ cacert/cacert_suite_test.go
@@ -0,0 +1,16 @@
+package cacert_test
+
+import (
+	"testing"
+
+	"github.com/keithpaterson/postal/logging"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestModels(t *testing.T) {
+	logging.Disable()
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Cacert Suite")
+}
